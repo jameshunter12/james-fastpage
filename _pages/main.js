f033c76ed438e794bf52522e4ce3d0f83883191c
T2fastpage <12james15545@gmail.com>
2023-02-05 20:48:37 -0800

    map test

--- _pages/main.js
+++ _pages/main.js
@@ -1,3 +1,8 @@
 const apiKey = 'pk.eyJ1IjoiamFtZXNodW50ZXIiLCJhIjoiY2xkcDJxdjkzMGZwYTNvbGJodWpjNmdlayJ9.bt4fm3yzeLvuxvMb8B6N8g';
 
 const map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
